0869ffdf3dcbce929aebc57e22667046dfb7df1e
Dudley Craig <dudleycraig@ip-10-0-0-166.us-east-2.compute.internal>
2022-09-29 19:02:54 +0200

    projecting toggled video tags onto 2d context canvas

--- src/App.js
+++ src/App.js
@@ -1,111 +1,67 @@
-import React, {useEffect, useRef, forwardRef, useState} from 'react';
-import THREE, {Material, MeshBasicMaterial, VideoTexture, Mesh, OrthographicCamera, PlaneGeometry, Scene, Vector4, WebGLRenderer} from 'three';
+import React, {useEffect, useRef, useState, forwardRef} from 'react';
+import {MeshBasicMaterial, VideoTexture, Mesh, OrthographicCamera, PlaneGeometry, Scene, WebGLRenderer} from 'three';
 import './index.css';
 
-const useAnimationFrame = (handler, duration = Number.POSITIVE_INFINITY, active = true) => {
-  const frame = useRef(0);
-  const firstFrameTime = useRef(performance.now());
+const Messages = ({messages, render = false, ...props}) => {
+  const map = {error: console.error, warning: console.warn, success: console.info};
+  return render 
+    ? <ul className="messages">{messages.map((message, index) => <li className={Object.keys(message)[0]} key={`message-${index}`}>{message[Object.keys(message)[0]]}</li>)}</ul>
+    : messages.map(message => map[Object.keys(message)[0]](message[Object.keys(message)[0]]));
+};
 
-  const animate = (now) => {
-    let timeFraction = (now - firstFrameTime.current) / duration;
-    if (timeFraction > 1) timeFraction = 1;
-    if (timeFraction <= 1) {
-      handler();
-      if (timeFraction != 1) frame.current = requestAnimationFrame(animate);
-    }
-  }
+const nextIndexForTransition = (transition, currentIndex) => ({next: currentIndex + 1, current: currentIndex, prev: currentIndex - 1}[transition]);
 
-  useEffect(() => {
-    if (active) {
-      firstFrameTime.current = performance.now();
-      frame.current = requestAnimationFrame(animate);
-    }
-    else {
-      cancelAnimationFrame(frame.current);
-    }
-    return () => cancelAnimationFrame(frame.current);
-  }, [active]);
-}
-
-const Messages = ({messages, ...props}) => (
-  <ul className="messages">
-    {messages.map((message, index) => 
-      <li className={Object.keys(message)[0]} key={`message-${index}`}>
-        {message[Object.keys(message)[0]]}
-      </li>
-    )}
-  </ul>
+const Video = forwardRef(({state, setState, ...props}, ref) =>  
+  <video 
+    className="video" style={{display: 'none'}} ref={ref}
+    muted={true} autoPlay={false} playsInline={true} loop={false} preload={'metadata'}
+    onWaiting={event => null}
+    onPlay={event => null}
+    onPlaying={event => null}
+    onPause={event => null}
+    onDurationChange={event => null}
+    onEmptied={event => null}
+    onLoadedData={event => null}
+    onLoadedMetadata={event => null}
+    onRateChange={event => null}
+    onSuspend={event => null}
+    onEnded={event => () => setState({...state, status: 'error'})} 
+    onTimeUpdate={event => state.status !== 'next' && event.target.duration - event.target.currentTime < 0.3 && setState({...state, status: 'next'})}
+    onError={(error) => setState({...state, status: 'error', messages: [{error: error.message}]})}
+  />
 );
 
 const App = (props) => {
-  const videoRef = useRef();
-  const canvasRef = useRef();
   const mediaRef = useRef();
-
+  const canvasRef = useRef();
+  const flopVideoRef = useRef();
+  const flipVideoRef = useRef();
   const [state, setState] = useState({
-    context: undefined,
     status: 'inert',
-    mode: 'webgl', // use either native video element for rendering, canvas renderer, or webgl renderer. ['video', 'canvas', 'webgl']
-    messages: [],
-    canvas: {
+    active: 'flip',
+    renderer: {
+      dimensions: undefined,
       context: undefined,
       interval: 1000 / 60,
-      intervalId: undefined,
-      dimensions: []
+      intervalId: undefined
     },
     sequence: {
       index: 0,
       clips: [
         {transition: 'next', file: 'https://cdn.cryptoys.dev/public/unboxing/zoofo/unboxing0_1920x1080.mp4'},
-        {transition: 'loop', file: 'https://cdn.cryptoys.dev/public/unboxing/zoofo/unboxing1_1920x1080.mp4'},
+        {transition: 'current', file: 'https://cdn.cryptoys.dev/public/unboxing/zoofo/unboxing1_1920x1080.mp4'},
         {transition: 'next', file: 'https://cdn.cryptoys.dev/public/unboxing/zoofo/unboxing2_1920x1080.mp4'}
       ]
     },
+    messages: []
   });
 
-  const initCanvas = () => {
-    const scale = window.devicePixelRatio;
-    const context = canvasRef.current.getContext('2d', {alpha: true});
-    context.scale(scale, scale);
-    context.fillStyle = '#000000';
-    return context;
-  }
-
-  const initWebGL = () => {
-    const scene = new Scene();
-    const camera = new OrthographicCamera(-1, 1, 1, -1, -1, 1);
-    const texture = new VideoTexture(videoRef.current);
-    const geometry = new PlaneGeometry(2, 2);
-    const material = new MeshBasicMaterial();
-    material.map = texture;
-
-    const plane = new Mesh(geometry, material);
-    scene.add(plane);
-
-    const context = new WebGLRenderer({canvas: canvasRef.current, alpha: true});
-    context.autoClearColor = true;
-    context.setClearColor(0x000000, 0);
-    context.setPixelRatio(devicePixelRatio);
-
-    useAnimationFrame({
-      handler: () => context.render(scene, camera),
-      active: true,
-      duration: 1000
-    });
-
-    return context;
-  }
-
-  useEffect(() => setState({...state, status: 'init'}), []);
-
   useEffect(() => {
-    if (videoRef.current) {
+    if (flipVideoRef.current) {
       switch (state.status) {
 
         case ('init'):
-          const {x, y, width, height, ...location} = mediaRef.current.getBoundingClientRect();
-          videoRef.current.style.width = `${width}px`;
-          videoRef.current.style.height = `${height}px`;
+          const {width, height} = mediaRef.current.getBoundingClientRect();
           canvasRef.current.style.width = `${width}px`;
           canvasRef.current.style.height = `${height}px`;
 
@@ -113,20 +69,11 @@ const App = (props) => {
           canvasRef.current.width = Math.floor(width * scale);
           canvasRef.current.height = Math.floor(height * scale);
 
-          setState({
-            ...state, 
-            status: 'loading', 
-            messages: [{success: 'media loading'}, ...state.messages],
-            canvas: {
-              ...state.canvas, 
-              dimensions: [0, 0, width, height], 
-              context: state.mode === 'webgl' 
-                ? initWebGL() 
-                : (state.mode === 'canvas' 
-                  ? initCanvas() 
-                  : undefined)
-            }
-          });
+          const context = canvasRef.current.getContext('2d', {alpha: false});
+          context.scale(scale, scale);
+          context.fillStyle = '#000000';
+
+          setState({...state, status: 'loading', renderer: {...state.renderer, context, dimensions: [0, 0, width, height]}});
           break;
 
         case ('loading'):
@@ -134,97 +81,94 @@ const App = (props) => {
             state.sequence.clips
               .map(clip => fetch(clip.file)
                 .then(response => response.blob().then(blob => URL.createObjectURL(blob)))))
-            .then(blobs => setState({...state, status: 'loaded', messages: [{success: 'media loaded'}, ...state.messages], sequence: {...state.sequence, clips: blobs.reduce((clips, blob, index) => [...clips, {...state.sequence.clips[index], blob}], [])}}))
-            .catch(error => setState({...state, status: 'error', messages: [{error: error.message}, ...state.messages]}));
+            .then(blobs => setState({...state, status: 'loaded', sequence: {...state.sequence, clips: blobs.reduce((clips, blob, index) => [...clips, {...state.sequence.clips[index], blob}], [])}}))
+            .catch(error => setState({...state, status: 'error', messages: [{error: error.message}]}));
           break;
 
         case ('loaded'):
-          setState({...state, status: 'playing', messages: [{success: `media playing clip ${state.sequence.index}`}, ...state.messages]});
+          flipVideoRef.current.src = state.sequence.clips[state.sequence.index].blob;
+          setState({...state, status: 'transition'});
+          break;
+
+        case ('next'):
+          setState({...state, status: 'transition', active: state.active === 'flop' ? 'flip' : 'flop', sequence: {...state.sequence, index: nextIndexForTransition(state.sequence.clips[state.sequence.index].transition, state.sequence.index)}});
+          break;
+
+        case ('transition'):
+          if (state.renderer.intervalId) clearInterval(state.renderer.intervalId);
+          setState({...state, status: 'play', renderer: {...state.renderer, intervalId: setInterval(() => state.renderer.context.drawImage((state.active === 'flip' ? flipVideoRef : flopVideoRef).current, ...state.renderer.dimensions), state.renderer.interval)}});
+          break;
+
+        case ('play'):
+          (state.active === 'flip' ? flipVideoRef : flopVideoRef).current.play();
+          setState({...state, status: 'playing'});
           break;
 
         case ('playing'):
-          videoRef.current.src = state.sequence.clips[state.sequence.index].blob;
-          videoRef.current.play();
-
-          if (state.mode === 'canvas') {
-            setState({
-              ...state, 
-              canvas: {
-                ...state.canvas, 
-                intervalId: window.setInterval(
-                  () => state.canvas.context.drawImage(videoRef.current, ...state.canvas.dimensions),
-                  state.canvas.interval
-                )
-              }
-            });
-          }
-          break;
-
-        case ('ended'):
-          if (state.mode === 'canvas') clearInterval(state.canvas.intervalId);
-          switch (state.sequence.clips[state.sequence.index].transition) {
-
-            case ('next'): 
-              setState({
-                ...state, 
-                status: 'playing', 
-                messages: [{success: `media playing next clip ${state.sequence.index}`}, ...state.messages], 
-                sequence: {...state.sequence, index: ++state.sequence.index}
-              });
-              break;
-
-            case ('prev'): 
-              setState({
-                ...state, 
-                status: 'playing', 
-                messages: [{success: `media playing prev clip ${state.sequence.index}`}, ...state.messages], 
-                sequence: {...state.sequence, index: --state.sequence.index}
-              });
-              break;
-
-            case ('loop'):
-              setState({
-                ...state, 
-                status: 'playing', 
-                messages: [{success: `media playing loop clip ${state.sequence.index}`}, ...state.messages]
-              });
-              break;
-          }
-          break;
-
-        default:
+          (state.active === 'flip' ? flopVideoRef : flipVideoRef).current.src = state.sequence.clips[nextIndexForTransition(state.sequence.clips[state.sequence.index].transition, state.sequence.index)].blob;
           break;
       }
     }
   }, [state.status]);
 
+  useEffect(() => setState({...state, status: 'init'}), []);
+
   return (
     <div className="layout">
       <div className="content">
         <div ref={mediaRef} className="media">
-          <canvas
-            className="canvas"
-            style={{display: (state.mode !== 'video' ? 'block' : 'none')}}
-            ref={canvasRef}
-          />
-          <video
-            className="video"
-            style={{display: (state.mode === 'video' ? 'block' : 'none')}}
-            ref={videoRef} 
-            muted={true} 
-            autoPlay={false} 
-            playsInline={true} 
-            loop={false} 
-            onPlay={() => setState({...state, status: 'playing'})}
-            onPause={() => setState({...state, status: 'paused'})}
-            onEnded={() => setState({...state, status: 'ended', messages: [{success: `media ended clip ${state.sequence.index}`}, ...state.messages]})}
-            onError={(error) => setState({...state, status: 'error', messages: [{error: error.message}, ...state.messages]})}
-          />
+          <canvas ref={canvasRef} className={'canvas'} />
+          <Video ref={flipVideoRef} state={state} setState={setState} />
+          <Video ref={flopVideoRef} state={state} setState={setState} />
         </div>
-        <Messages messages={state.messages} />
+        <Messages messages={state.messages} render={false} />
       </div>
     </div>
   );
 }
 
 export default App;
+
+/**
+  canvasRef.current.style.width = `${width}px`;
+  canvasRef.current.style.height = `${height}px`;
+
+  const scale = window.devicePixelRatio;
+  canvasRef.current.width = Math.floor(width * scale);
+  canvasRef.current.height = Math.floor(height * scale);
+
+  const scene = new Scene();
+  const camera = new OrthographicCamera(-1, 1, 1, -1, -1, 1);
+  const texture = new VideoTexture(flipVideoRef.current);
+  const geometry = new PlaneGeometry(2, 2);
+  const material = new MeshBasicMaterial();
+  material.map = texture;
+
+  const plane = new Mesh(geometry, material);
+  scene.add(plane);
+
+  const context = new WebGLRenderer({canvas: canvasRef.current, alpha: true});
+  context.autoClearColor = true;
+  context.setClearColor(0x000000, 0);
+  context.setPixelRatio(devicePixelRatio);
+  context.render(scene, camera);
+
+  setState({...state, status: 'loading', context, messages: [{success: 'media loading'}]});
+ **/
+
+/**
+  const useAnimationFrame = (handler) => {
+    const frame = useRef(0);
+
+    useEffect(() => {
+      const animate = () => {
+        handler();
+        frame.current = requestAnimationFrame(animate);
+      }
+
+      frame.current = requestAnimationFrame(animate);
+      return () => cancelAnimationFrame(frame.current);
+    }, [handler]);
+  };
+ **/
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/** 
+ * strict mode removed due react 18 bug, 
+ * https://www.techiediaries.com/react-18-useeffect/ 
+ **/
+root.render(<App />);
