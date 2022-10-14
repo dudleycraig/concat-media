01d995c3c8694f2f908b426a2d43e4cfc93b3057
Dudley Craig <dudleycraig@ip-10-0-0-166.us-east-2.compute.internal>
2022-10-14 15:47:29 +0200

    video rendering on canvas

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React, {useEffect, useRef, useState, forwardRef} from 'react';
-import {MeshBasicMaterial, VideoTexture, Mesh, OrthographicCamera, PlaneGeometry, Scene, WebGLRenderer} from 'three';
+import React, {useEffect, useRef, useState} from 'react';
 import './index.css';
 import useAnimationFrame from './hooks/useAnimationFrame';
 import Messages from './components/Messages';
 import Video from './components/Video';
-
-const init = (mediaRef, canvasRef, flipRef) => {
-  const {width, height} = mediaRef.current.getBoundingClientRect();
-  canvasRef.current.style.width = `${width}px`;
-  canvasRef.current.style.height = `${height}px`;
-
-  const scale = window.devicePixelRatio;
-  canvasRef.current.width = Math.floor(width * scale);
-  canvasRef.current.height = Math.floor(height * scale);
-
-  const scene = new Scene();
-  const camera = new OrthographicCamera(-1, 1, 1, -1, -1, 1);
-  const texture = new VideoTexture(flipRef.current);
-  const geometry = new PlaneGeometry(2, 2);
-  const material = new MeshBasicMaterial();
-  material.map = texture;
-
-  const plane = new Mesh(geometry, material);
-  scene.add(plane);
-
-  const context = new WebGLRenderer({canvas:canvasRef.current, alpha:true});
-  context.autoClearColor = true;
-  context.setClearColor(0x000000, 0);
-  context.setPixelRatio(devicePixelRatio);
-
-  return {context, dimensions:[0, 0, width, height], scene, camera};
-}
+import renderer from './renderer';
 
 const App = props => {
   const [state, setState] = useState({
     status:'inert',
-    players:{active:'flip', flip:undefined, flop:undefined},
     renderer:{dimensions:undefined, context:undefined, scene:undefined, camera:undefined},
     sequence:{
       index:0, 
@@ -48,16 +20,25 @@ const App = props => {
   });
   const mediaRef = useRef();
   const canvasRef = useRef();
-  const flipRef = useRef();
-  const flopRef = useRef();
-  const reducer = (state, { time }) => {
-    if (!mediaRef.current || !canvasRef.current || !flipRef.current || !flopRef.current) return state;
+  const videoRef = useRef();
+
+  const reducer = (state, {time}) => {
+    if (!mediaRef.current || !canvasRef.current || !videoRef.current) return state;
+
+    console.log(`status: ${state.status}`);
     switch (state.status) {
-      case ('inert'):
+      case ('inert'): 
         return state;
 
-      case ('init'):
-        return {...state, status:'requesting-clips', players:{...state.players, active:'flip', flip:flipRef.current, flop:flopRef.current}};
+      case ('init'): 
+        return {
+          ...state, 
+          status:'requesting-clips', 
+          renderer: {
+            ...state.renderer,
+            ...renderer.init(mediaRef, canvasRef, videoRef)
+          }
+        };
 
       case ('requesting-clips'):
         Promise.all(
@@ -66,61 +47,108 @@ const App = props => {
               .then(response => response.blob().then(blob => URL.createObjectURL(blob)))))
           .then(blobs => setState(state => ({...state, status:'clips-response-received', sequence:{...state.sequence, clips:blobs.reduce((clips, blob, index) => [...clips, {...state.sequence.clips[index], blob}], [])}})))
           .catch(error => setState(state => ({...state, status:'error', messages:[{error:error.message}]})));
-        return {...state, status:'waiting-clips-response'};
+        return {
+          ...state, 
+          status:'waiting-clips-response'
+        };
 
-      case ('waiting-clips-response'):
+      case ('waiting-clips-response'): 
         return state;
 
-      case ('clips-response-received'):
-        return {...state, status:'load-next-clip'};
+      case ('clips-response-received'): 
+        return {
+          ...state, 
+          status:'load-next-clip'
+        };
 
-      case ('transition-next-clip'):
-        return {...state, status:'load-next-clip', players:{...state.players, active:'flip'}, sequence:{...state.sequence, index:{next:state.sequence.index + 1, current:state.sequence.index, prev:state.sequence.index - 1}[state.sequence.clips[state.sequence.index].transition], clips:[...(state.sequence.index > 0 ? state.sequence.clips.slice(0, state.sequence.index) : []), {...state.sequence.clips[state.sequence.index], started:0}, ...(state.sequence.clips.length > (state.sequence.index + 1) ? state.sequence.clips.slice(state.sequence.index + 1) : [])]}};
+      case ('transition-next-clip'): 
+        return {
+          ...state, 
+          status:'load-next-clip', 
+          sequence:{
+            ...state.sequence, 
+            index:{next:state.sequence.index + 1, current:state.sequence.index, prev:state.sequence.index - 1}[state.sequence.clips[state.sequence.index].transition], 
+            clips:[
+              ...(state.sequence.index > 0 ? state.sequence.clips.slice(0, state.sequence.index) : []), 
+              {...state.sequence.clips[state.sequence.index], started:0}, 
+              ...(state.sequence.clips.length > (state.sequence.index + 1) ? state.sequence.clips.slice(state.sequence.index + 1) : [])
+            ]
+          }
+        };
 
-      case ('load-next-clip'):
-        state.players[state.players.active].src = state.sequence.clips[state.sequence.index].blob;
-        return {...state, status:'loading-next-clip'};
+      case ('load-next-clip'): 
+        videoRef.current.src = state.sequence.clips[state.sequence.index].blob;
+        return {
+          ...state, 
+          status:'loading-next-clip'
+        };
 
-      case ('loading-next-clip'):
+      case ('loading-next-clip'): 
         return state;
 
-      case ('loaded-next-clip'):
-        return {...state, status:'play-clip'};
+      case ('loaded-next-clip'): 
+        return {
+          ...state, 
+          status:'play-clip'
+        };
 
       case ('play-clip'):
-        state.players[state.players.active].play();
-        return {...state, status:'playing-clip', sequence:{...state.sequence, clips:[...(state.sequence.index > 0 ? state.sequence.clips.slice(0, state.sequence.index) : []), {...state.sequence.clips[state.sequence.index], started:Date.now()}, ...(state.sequence.clips.length > (state.sequence.index + 1) ? state.sequence.clips.slice(state.sequence.index + 1) : [])]}};
+        videoRef.current.play();
+        return {
+          ...state, 
+          status:'playing-clip', 
+          sequence:{
+            ...state.sequence, 
+            clips:[
+              ...(state.sequence.index > 0 ? state.sequence.clips.slice(0, state.sequence.index) : []), 
+              {...state.sequence.clips[state.sequence.index], started:Date.now()}, 
+              ...(state.sequence.clips.length > (state.sequence.index + 1) ? state.sequence.clips.slice(state.sequence.index + 1) : [])
+            ]
+          }
+        };
 
-      case ('playing-clip'):
-        return {...state, status:state.sequence.clips[state.sequence.index].duration - (Date.now() - state.sequence.clips[state.sequence.index].started) < 100 ? 'transition-next-clip' : 'playing-clip'};
-
-      default:
-        return {...state, status:'error', messages:[{error:`No defined handler for status '${state.status}'`}]};
+      case ('playing-clip'): 
+        return {
+          ...state, 
+          status:state.sequence.clips[state.sequence.index].duration - (Date.now() - state.sequence.clips[state.sequence.index].started) < 20 ? 'transition-next-clip' : 'playing-clip'
+        };
 
       case ('ended-clips'):
-      case ('error'):
-        return {...state, status:'stop'};
+      case ('error'): 
+        return {
+          ...state, 
+          status:'stop'
+        };
 
-      case ('stop'):
+      case ('stop'): 
         return state;
+
+      default: 
+        return {
+          ...state, 
+          status:'error', 
+          messages:[
+            {error:`No defined handler for status '${state.status}'`}
+          ]
+        };
     }
   }
 
   useAnimationFrame(time => {
     setState(state => reducer(state, time));
+    if (state.renderer.context) renderer.draw(state.renderer); 
   });
 
   useEffect(() => {
-    setState({...state, status:'init'});
+    setState(state => ({...state, status:'init'}));
   }, []);
 
   return (
     <div className="layout">
       <div className="content">
         <div ref={mediaRef} className="media">
-          <canvas ref={canvasRef} className={'canvas'} style={{display: 'none'}} />
-          <Video ref={flipRef} setState={setState} active={true} />
-          <Video ref={flopRef} setState={setState} active={false} />
+          <canvas ref={canvasRef} className={'canvas'} />
+          <Video ref={videoRef} setState={setState} active={false} />
         </div>
         <Messages messages={state.messages} render={false} />
       </div>
